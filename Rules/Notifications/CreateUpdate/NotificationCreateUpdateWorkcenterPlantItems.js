8065a67f24c01b05de2205df2514d907ca67c334
shig <david.shi@delaware.pro>
2024-07-26 10:24:21 +0800

    workcenter联动

---
+++ Rules/Notifications/CreateUpdate/NotificationCreateUpdateWorkcenterPlantItems.js
@@ -0,0 +1,25 @@
+import updateGroupPickers from './UpdateGroupPickers';
+import libVal from '../../Common/Library/ValidationLibrary';
+import updateSoldToPartyPickers from './NotificationCreateUpdateSoldToPartyPickers';
+import Logger from '../../Log/Logger';
+
+export default function NotificationCreateUpdateWorkcenterPlantItems(clientAPI, controlProxy) {
+    let formCellContainer = clientAPI.getControl('FormCellContainer');
+    //let formCellContainer = context.getControl('FormCellContainer');
+    let value = formCellContainer.getControl('MainWorkCenterListPicker').getValue();
+    filter = "$filter=WorkcenterId eq '" + "111" + "'" + "&$orderby=PlantId";
+    return controlProxy.read('/SAPAssetManager/Services/AssetManager.service', 'WorkCenters', [], filter).then(function(obArray) {
+        var jsonResult = [];
+        obArray.forEach(function(element) {
+            jsonResult.push(
+                {
+                    'DisplayValue': `${element.PlantId} - ${element.WorkCenterName}`,
+                    'ReturnValue': element.PlantId,
+                });
+        });
+        const uniqueSet = new Set(jsonResult.map(item => JSON.stringify(item)));
+        let finalResult = [...uniqueSet].map(item => JSON.parse(item));
+        return finalResult;
+    });
+   
+}
