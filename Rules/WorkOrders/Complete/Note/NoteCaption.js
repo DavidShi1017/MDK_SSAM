d1883664baa4a593462b903760217fe0202cb367
shig <david.shi@delaware.pro>
2024-08-13 13:09:20 +0800

    add check for complete

--- Rules/WorkOrders/Complete/Note/NoteCaption.js
+++ Rules/WorkOrders/Complete/Note/NoteCaption.js
@@ -8,6 +8,14 @@ export default function NoteCaption(context) {
         if (isReviewRequired) {
             return context.localizeText('notes');
         } else {
+            if(binding.WOHeader){
+                let orderType = binding.WOHeader.OrderType;
+                if('KM01' === orderType){
+                    return context.localizeText('notes');
+                }else{
+                    return context.localizeText('supervisor_name');
+                }
+            }
             return context.localizeText('supervisor_name');
         }
     });
