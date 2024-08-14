af04ec451f432e54662c8e3f8423bcc6ff7b0694
shig <david.shi@delaware.pro>
2024-08-14 16:35:10 +0800

    add plant for Filters

--- Rules/Inventory/MaterialDocument/FilterTypeWorkcent.js
+++ Rules/Inventory/MaterialDocument/FilterTypeWorkcent.js
@@ -11,7 +11,7 @@ export default function FilterTypeWorkcent(context) {
     return context.read('/SAPAssetManager/Services/AssetManager.service', 'Plants', [], queryOptions).then((result) => {
         if (result && result.length) {
             let item = result.getItem(0);
-            return `${workcenterId} - ${item.PlantId} - ${item.PlantDescription}`;
+            return `${workcenterId} - ${plant} - ${item.PlantDescription}`;
         }
         return '-';
     });
