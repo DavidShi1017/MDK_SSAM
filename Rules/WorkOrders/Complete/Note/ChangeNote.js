8d333d32d0112724b8626a109fe35dc154dba66a
shig <david.shi@delaware.pro>
2024-08-14 16:52:18 +0800

    fix bug

--- Rules/WorkOrders/Complete/Note/ChangeNote.js
+++ Rules/WorkOrders/Complete/Note/ChangeNote.js
@@ -28,7 +28,8 @@ export default function ChangeNote(context) {
         if (note && note.NewTextString) {
             libCommon.setStateVariable(context, Constants.noteStateVariable, note); 
             let page = context.currentPage._resolvedCaption;
-            if(page === context.localizeText('complete_operation')){
+            
+            if(context.binding.WOHeader.OrderType === 'KM05'){
                 return context.executeAction('/SAPAssetManager/Actions/Notes/NoteNameUpdateNav.action');
             }else{
                 return context.executeAction('/SAPAssetManager/Actions/Notes/NoteUpdateNav.action');
@@ -38,7 +39,7 @@ export default function ChangeNote(context) {
             libCommon.setOnCreateUpdateFlag(context, 'CREATE');
             libCommon.setOnChangesetFlag(context, false);
             let page = context.currentPage._resolvedCaption;
-            if(page === context.localizeText('complete_operation')){
+            if(context.binding.WOHeader.OrderType === 'KM05'){
                 return context.executeAction('/SAPAssetManager/Actions/Notes/NoteNameCreateNav.action');
             }else{
                 return context.executeAction('/SAPAssetManager/Actions/Notes/NoteCreateNav.action');
