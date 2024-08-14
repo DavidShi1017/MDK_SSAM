b1007e5e3f27f7e59f4ecc619fe06e6e477b9dde
shig <david.shi@delaware.pro>
2024-08-14 14:34:20 +0800

    add plant for Filters

---
+++ Rules/Common/FilterTypeValue.js
@@ -0,0 +1,26 @@
+import libVal from './Library/ValidationLibrary';
+import Logger from '../Log/Logger';
+
+export default function FilterTypeValue(context) {
+    let binding = context.binding;
+    try {
+
+        let plant = '';
+
+        if (context.getValue().length > 0) {
+            plant = context.getValue()[0].ReturnValue;
+            let plannerGroupListPicker = context.getPageProxy().evaluateTargetPathForAPI('#Control:MainWorkCenterListPicker');
+            let plannerGroupListPickerSpecifier = plannerGroupListPicker.getTargetSpecifier();
+            let plannerGroupQueryOptions = `$orderby=PlantId&$filter=PlantId eq '${plant}' `;
+            plannerGroupListPickerSpecifier.setEntitySet('WorkCenters');
+            plannerGroupListPickerSpecifier.setQueryOptions(plannerGroupQueryOptions);
+            plannerGroupListPickerSpecifier.setReturnValue('{WorkCenterId}');
+            plannerGroupListPickerSpecifier.setDisplayValue('{ExternalWorkCenterId} - {PlantId}');
+            plannerGroupListPicker.setTargetSpecifier(plannerGroupListPickerSpecifier);
+        }
+        return true;
+    } catch (err) {
+        /**Implementing our Logger class*/
+        Logger.error(context.getGlobalDefinition('/SAPAssetManager/Globals/Logs/CategoryParts.global').getValue(),`PartLibrary.partCreateUpdateOnChange(PlantLstPkr) error: ${err}`);
+    }
+}
