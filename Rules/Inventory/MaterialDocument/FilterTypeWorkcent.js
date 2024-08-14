343843797529449ce4183d16f68585e8b2209dab
shig <david.shi@delaware.pro>
2024-08-14 16:23:53 +0800

    add plant for Filters

--- Rules/Inventory/MaterialDocument/FilterTypeWorkcent.js
+++ Rules/Inventory/MaterialDocument/FilterTypeWorkcent.js
@@ -6,15 +6,13 @@ import libCommon from '../../Common/Library/CommonLibrary';
 export default function FilterTypeWorkcent(context) {
     let binding = context.binding;
     let plant = binding.PlantId;
-    let query = `$filter=PlantId eq '${plant}'`;
+    var queryOptions = "$filter=Plant eq '" + plant +"'";
     let workcenterId = binding.ExternalWorkCenterId;
-    return context.read('/SAPAssetManager/Services/AssetManager.service', 'Plants', [], query).then(data => {
-        if (data && data.length) {
-            let item = data.getItem(0);
+    return context.read('/SAPAssetManager/Services/AssetManager.service', 'Plants', [], queryOptions).then((result) => {
+        if (result && result.length) {
+            let item = result.getItem(0);
             return `${workcenterId} - ${item.PlantId} - ${item.PlantDescription}`;
         }
         return '-';
     });
-    
-    
 }
