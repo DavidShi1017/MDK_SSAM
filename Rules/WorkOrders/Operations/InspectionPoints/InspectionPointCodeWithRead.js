888294d67ae08a4372a82959d67e3413d6f43303
shig <david.shi@delaware.pro>
2024-08-21 09:56:52 +0800

    patch --no-backup-if-mismatch -p4 < path/to/3507838.patch

--- Rules/WorkOrders/Operations/InspectionPoints/InspectionPointCodeWithRead.js
+++ Rules/WorkOrders/Operations/InspectionPoints/InspectionPointCodeWithRead.js
@@ -1,14 +1,10 @@
 /**
-* Describe this function...
+* Reads the Inspection Point Code Description
 * @param {IClientAPI} context
 */
 export default function InspectionPointCodeWithRead(context) {
     return context.read('/SAPAssetManager/Services/AssetManager.service', context.binding['@odata.readLink'], [], '$expand=InspCode_Nav').then(result => {
-        if (result && result.length > 0) {
-            return result.getItem(0).InspCode_Nav.CodeDesc;
-        } else {
-            return '-';
-        }
+        return result.getItem(0)?.InspCode_Nav?.CodeDesc || '-';
     }).catch(() => {
         return '-';
     });
