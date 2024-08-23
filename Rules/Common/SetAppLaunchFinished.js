a4373db5638ee6eea5c8bdb0e92a4a5123d4dc26
shig <david.shi@delaware.pro>
2024-08-23 14:09:17 +0800

    restore the AutoSyncLibrary

--- Rules/Common/SetAppLaunchFinished.js
+++
@@ -1,5 +0,0 @@
-import ApplicationSettings from './Library/ApplicationSettings';
-
-export default function SetAppLaunchFinished(context) {
-    return ApplicationSettings.setBoolean(context, 'onAppLaunch', false);
-}
--- Rules/Common/SetAppLaunchStarted.js
+++
@@ -1,5 +0,0 @@
-import ApplicationSettings from './Library/ApplicationSettings';
-
-export default function SetAppLaunchStarted(context) {
-    return ApplicationSettings.setBoolean(context, 'onAppLaunch', true);
-}
