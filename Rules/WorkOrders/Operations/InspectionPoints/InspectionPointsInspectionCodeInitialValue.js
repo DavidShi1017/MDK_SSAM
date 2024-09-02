6070bbe2a03bf22b8c71a162e8dabe086e678438
shig <david.shi@delaware.pro>
2024-09-02 14:49:49 +0800

    set default value for InspectionPoints

--- Rules/WorkOrders/Operations/InspectionPoints/InspectionPointsInspectionCodeInitialValue.js
+++ Rules/WorkOrders/Operations/InspectionPoints/InspectionPointsInspectionCodeInitialValue.js
@@ -20,11 +20,41 @@ export default async function InspectionPointsInspectionCodeInitialValue(context
             }
         });
         const queryOptions = await InspectionLotSetUsageQueryOptions(context);
-
+        let sortedItems ;
+        sortedItems = await context.read('/SAPAssetManager/Services/AssetManager.service', 'InspectionCodes', [], queryOptions).then((result) => {
+            sortedItems = [...result].sort((a,b) => {
+                const codeA = a.Code;
+                const codeB = b.Code;
+    
+                // compare as strings if one of items not numeric or or numeric with prefix 0 ('0001' and '1'), otherwise compare as numbers
+                if (`${+codeA}` !== codeA || `${+codeB}` !== codeB) {
+                    return codeA.localeCompare(codeB);
+                } else {
+                    return codeA - codeB;
+                }
+            });
+    
+            return sortedItems;
+        });
+        let ClientData = {};
         if(isPass){
-
+            ClientData.Valuation = sortedItems.getItem(0).ValuationStatus;
+            ClientData.ValSelectedSet=sortedItems.getItem(0).SelectedSet;
+            ClientData.ValCatalog=sortedItems.getItem(0).Catalog;
+            ClientData.ValCode=sortedItems.getItem(0).Code;
+            ClientData.ValCodeGroup=sortedItems.getItem(0).CodeGroup;
+            ClientData.Plant=sortedItems.getItem(0).Plant;
+            binding.ClientData = ClientData;
+            return libCommon.decodeReadLink(sortedItems.getItem(0)['@odata.readLink']);
         }else{
-
+            ClientData.Valuation = sortedItems.getItem(1).ValuationStatus;
+            ClientData.ValSelectedSet=sortedItems.getItem(1).SelectedSet;
+            ClientData.ValCatalog=sortedItems.getItem(1).Catalog;
+            ClientData.ValCode=sortedItems.getItem(1).Code;
+            ClientData.ValCodeGroup=sortedItems.getItem(1).CodeGroup;
+            ClientData.Plant=sortedItems.getItem(1).Plant;
+            binding.ClientData = ClientData;
+            return libCommon.decodeReadLink(sortedItems.getItem(1)['@odata.readLink']);
         }
     }
 }
