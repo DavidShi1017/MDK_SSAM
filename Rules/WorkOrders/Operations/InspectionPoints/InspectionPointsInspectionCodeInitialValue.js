5b44551cf0b38f9a841541c78174778788641009
shig <david.shi@delaware.pro>
2024-09-02 14:07:30 +0800

    set default value for InspectionPoints

--- Rules/WorkOrders/Operations/InspectionPoints/InspectionPointsInspectionCodeInitialValue.js
+++ Rules/WorkOrders/Operations/InspectionPoints/InspectionPointsInspectionCodeInitialValue.js
@@ -1,6 +1,6 @@
 import libVal from '../../../Common/Library/ValidationLibrary';
 
-export default function InspectionPointsInspectionCodeInitialValue(context) {
+export default async function InspectionPointsInspectionCodeInitialValue(context) {
     if (Object.prototype.hasOwnProperty.call(context.binding,'InspCode_Nav') && !libVal.evalIsEmpty(context.binding.InspCode_Nav)) {
         let ClientData = {};
         ClientData.Valuation = context.binding.InspCode_Nav.ValuationStatus;
@@ -11,5 +11,20 @@ export default function InspectionPointsInspectionCodeInitialValue(context) {
         ClientData.Plant=context.binding.InspCode_Nav.Plant;
         context.binding.ClientData = ClientData;
         return context.binding.InspCode_Nav['@odata.readLink'];
+    }else{
+        let inspectionChar = context.binding.InspectionChar_Nav;
+        let isPass = true;
+        inspectionChar.map((item) => {
+            if(item.Valuation !== 'A'){
+                isPass = false;
+            }
+        });
+        const queryOptions = await InspectionLotSetUsageQueryOptions(context);
+
+        if(isPass){
+
+        }else{
+
+        }
     }
 }
