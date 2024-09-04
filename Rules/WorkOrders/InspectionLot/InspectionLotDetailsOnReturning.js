f782b4b5ea137a2029bc12584c2f1f4f3405b058
shig <david.shi@delaware.pro>
2024-09-04 15:25:50 +0800

    set default value for InspectionPoints

--- Rules/WorkOrders/InspectionLot/InspectionLotDetailsOnReturning.js
+++ Rules/WorkOrders/InspectionLot/InspectionLotDetailsOnReturning.js
@@ -5,6 +5,7 @@ export default function InspectionLotDetailsOnReturning(context) {
     if (context.binding['@odata.type'] === '#sap_mobile.InspectionLot') {
         return context.read('/SAPAssetManager/Services/AssetManager.service', context.binding['@odata.readLink'], [], '$expand=InspectionPoints_Nav').then ((inspectionLotObject) => {
             if (inspectionLotObject.length > 0) {
+                context.evaluateTargetPathForAPI('#Page:WorkOrderOperationDetailsPage').getClientData().inspectionPointNav = inspectionLotObject;
                 context._context.binding = inspectionLotObject.getItem(0);
                 return context.setActionBarItemVisible(0, EnableInspectionLotSetUsage(context));
             } else {
