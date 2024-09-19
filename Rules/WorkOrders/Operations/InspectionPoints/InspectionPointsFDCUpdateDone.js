d4e0305b43d478837a32bef89cf515d43187d97c
shig <david.shi@delaware.pro>
2024-09-19 14:41:35 +0800

    try to cancel InspectionCharacteristics

--- Rules/WorkOrders/Operations/InspectionPoints/InspectionPointsFDCUpdateDone.js
+++ Rules/WorkOrders/Operations/InspectionPoints/InspectionPointsFDCUpdateDone.js
@@ -90,6 +90,7 @@ export default function InspectionPointsFDCUpdateDone(context) {
                     'ValidationRule': '',
                     'CreateLinks': createLinks,
                     'UpdateLinks': updateLinks,
+                    'DeleteLinks':  `InspectionCodes(Plant='${plant}',SelectedSet='${valSelectedSet}',Catalog='${valCatalog}',CodeGroup='${valCodeGroup}',Code='${valCode}')`,
                 }}).catch(() => {
                     return Promise.resolve();
                 });
