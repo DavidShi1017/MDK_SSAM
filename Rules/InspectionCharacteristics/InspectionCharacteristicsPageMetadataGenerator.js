1ebeedef17c1162ccfb59a32383dddde7377c8d8
shig <david.shi@delaware.pro>
2024-12-03 11:31:33 +0800

    解决operation页面进入inspection points页面没有保存本地数据问题

--- Rules/InspectionCharacteristics/InspectionCharacteristicsPageMetadataGenerator.js
+++ Rules/InspectionCharacteristics/InspectionCharacteristicsPageMetadataGenerator.js
@@ -37,8 +37,13 @@ export default async function InspectionCharacteristicsPageMetadataGenerator(cli
         }
     } else if (binding['@odata.type'] === '#sap_mobile.MyWorkOrderOperation') {
         entityset = binding['@odata.readLink'] + '/InspectionPoint_Nav';
-    } else if (binding['@odata.type'] === '#sap_mobile.InspectionPoint' || binding['@odata.type'] === '#sap_mobile.EAMChecklistLink') {
-        entityset = binding['@odata.readLink'];
+    } else if (binding['@odata.type'] === '#sap_mobile.InspectionPoint' || binding['@odata.type'] === '#sap_mobile.EAMChecklistLink') {      
+        if(binding.InspectionLot_Nav["@odata.type"] === '#sap_mobile.InspectionLot'){
+            libCom.setStateVariable(clientAPI, binding.OrderId + "-" + binding.InspectionLot, binding.WOHeader_Nav);
+            entityset = binding['@odata.readLink'] + '/InspectionPoints_Nav';
+        }else{
+            entityset = binding['@odata.readLink'];
+        }
     }
     page.Controls[0].Sections = [];
     if (entityset) {
