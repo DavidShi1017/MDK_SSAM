f17699a0c652ecb6b63da04d5eef50eff5c1ab62
Shi <shig@delawareconsulting.com>
2025-01-22 17:13:42 +0800

    CR 44 Not Running

--- Rules/InspectionCharacteristics/InspectionCharacteristicsNotRunningItem.js
+++ Rules/InspectionCharacteristics/InspectionCharacteristicsNotRunningItem.js
@@ -49,7 +49,7 @@ export default async function InspectionCharacteristicsNotRunningItem(context) {
         //valuationCell.setValue(0);
         let listPickerValue = '';
         let listPickerDisplayValue = '';
-        let filter = '$orderby=Code asc&$filter=(SelectedSet eq \'' + binding.SelectedSet + '\' and Plant eq \'' + binding.SelectedSetPlant + '\' and Catalog eq \'' + binding.Catalog + '\' and Code eq N0\')';
+        let filter = '$orderby=Code asc&$filter=(SelectedSet eq \'' + binding.SelectedSet + '\' and Plant eq \'' + binding.SelectedSetPlant + '\' and Catalog eq \'' + binding.Catalog + '\' and Code eq \'N0\')';
         let CodeGroup = await context.read('/SAPAssetManager/Services/AssetManager.service', 'InspectionCodes', [], filter).then(valuationResult => {
             if (valuationResult && valuationResult.getItem(0)) {
                 return valuationResult.getItem(0).ShortText;
