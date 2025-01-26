2d29eee0a784f75efd4b71e04175c6dc9ee1b579
Shi <shig@delawareconsulting.com>
2025-01-26 10:33:38 +0800

    CR 44 Not Running

--- Rules/InspectionCharacteristics/Update/InspectionCharacteristicsQuantitativeAndQualitativeEDTControls.js
+++ Rules/InspectionCharacteristics/Update/InspectionCharacteristicsQuantitativeAndQualitativeEDTControls.js
@@ -74,13 +74,14 @@ export default function InspectionCharacteristicsQuantitativeAndQualitativeEDTCo
     } else if (inspCharLib.isQualitative(binding)) {
         let listPickerValue = '';
         let listPickerDisplayValue = '';
+        libCom.setStateVariable(context, 'SelectedSet', binding.SelectedSet);
+        libCom.setStateVariable(context, 'SelectedSetPlant', binding.SelectedSetPlant);
+        libCom.setStateVariable(context, 'Catalog', binding.Catalog);
         if(libVal.evalIsEmpty(binding.Valuation)){
             //listPickerValue = `InspectionCodes(Plant='${binding.SelectedSetPlant}',SelectedSet='${binding.SelectedSet}',Catalog='${binding.Catalog}',CodeGroup='${binding.CodeGroup}',Code='${binding.Code}')`;
             //isMandatory = true;
         }else{
-            libCom.setStateVariable(context, 'SelectedSet', binding.SelectedSet);
-            libCom.setStateVariable(context, 'SelectedSetPlant', binding.SelectedSetPlant);
-            libCom.setStateVariable(context, 'Catalog', binding.Catalog);
+
             if (!libVal.evalIsEmpty(binding.InspectionCode_Nav) && !libVal.evalIsEmpty(binding.InspectionCode_Nav.CodeDesc)) {
                 //if (!libVal.evalIsEmpty(binding.SelectedSetPlant) && !libVal.evalIsEmpty(binding.SelectedSet) && !libVal.evalIsEmpty(binding.Catalog) && !libVal.evalIsEmpty(binding.CodeGroup) && !libVal.evalIsEmpty(binding.Code)) {
                     listPickerValue = `InspectionCodes(Plant='${binding.SelectedSetPlant}',SelectedSet='${binding.SelectedSet}',Catalog='${binding.Catalog}',CodeGroup='${binding.CodeGroup}',Code='${binding.Code}')`;
