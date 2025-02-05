b3c8aace5f60d3c1908c1efffe6a628b1dff9ae3
Shi <shig@delawareconsulting.com>
2025-02-05 09:10:51 +0800

    CR 44 Not Running

--- Rules/InspectionCharacteristics/InspectionCharacteristicsPageMetadataGenerator.js
+++ Rules/InspectionCharacteristics/InspectionCharacteristicsPageMetadataGenerator.js
@@ -174,7 +174,7 @@ export function addHeaderSection(context, binding, page) {
 
                 if (result.getItem(0)['@odata.type'] === '#sap_mobile.MyEquipment') {
                     headlineText = result.getItem(0).EquipId + ' - ' + result.getItem(0).EquipDesc;
-                    subhead = 'Tech ID: ' + result.getItem(0).TechnicalID + '  ';
+                    subhead = 'Tech ID: ' + result.getItem(0).TechnicalID + '       ';
                 } else if (result.getItem(0)['@odata.type'] === '#sap_mobile.MyFunctionalLocation') {
                     headlineText =  result.getItem(0).FuncLocId + ' - ' + result.getItem(0).FuncLocDesc;
                 }
