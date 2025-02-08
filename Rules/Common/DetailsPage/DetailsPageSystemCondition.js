5a40f05b3ee32ce95d9f838ae76c2d6a0ac1eae7
Shi <shig@delawareconsulting.com>
2025-02-08 16:05:58 +0800

    CR 37 Shutdown function

--- Rules/Common/DetailsPage/DetailsPageSystemCondition.js
+++ Rules/Common/DetailsPage/DetailsPageSystemCondition.js
@@ -1,27 +1,12 @@
 import commonLib from '../Library/CommonLibrary';
 
 export default function DetailsPageSystemCondition(context) {
-    const entityType = commonLib.getEntitySetName(context);
     let statusObject;
     let binding = context.binding;
-    switch (entityType) {
-        case 'MyWorkOrderHeaders':
-            statusObject = context.binding.OrderMobileStatus_Nav;
-            break;
-        case 'MyWorkOrderOperations':
-            statusObject = context.binding.OperationMobileStatus_Nav;
-            break;
-        case 'MyNotificationHeaders':
-            statusObject = context.binding.NotifMobileStatus_Nav;
-            break;
-        default:
-            break;
+    if(binding.ZSystemCondition){
+        return binding.ZSystemCondition + " - Shutdown";
     }
 
-    return {
-        UserStatusText: 'X -',
-        SystemStatusText: results[1].join(', ') || '-',
-    };
-
+    return '';
 }
 
