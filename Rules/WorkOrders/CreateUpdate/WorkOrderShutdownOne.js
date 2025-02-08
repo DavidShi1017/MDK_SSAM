578942c6f5df1af8c124f74d278d399c4e7af8ec
Shi <shig@delawareconsulting.com>
2025-02-08 13:53:37 +0800

    CR 37 Shutdown function

--- Rules/WorkOrders/CreateUpdate/WorkOrderShutdownOne.js
+++ Rules/WorkOrders/CreateUpdate/WorkOrderShutdownOne.js
@@ -8,6 +8,6 @@ export default function WorkOrderShutdownOne(clientAPI) {
     let binding = clientAPI.binding;
 
     if(binding){
-
+        return clientAPI.executeAction('/SAPAssetManager/Actions/WorkOrders/CreateUpdate/WorkOrderShutdown.action');
     }
 }
