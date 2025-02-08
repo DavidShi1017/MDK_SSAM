a106d2441bb95f390dcbe140fb966f846f6166c8
Shi <shig@delawareconsulting.com>
2025-02-08 16:33:42 +0800

    CR 37 Shutdown function

--- Rules/Common/DetailsPage/DetailsPageSystemCondition.js
+++ Rules/Common/DetailsPage/DetailsPageSystemCondition.js
@@ -3,10 +3,10 @@ import commonLib from '../Library/CommonLibrary';
 export default function DetailsPageSystemCondition(context) {
     let statusObject;
     let binding = context.binding;
-    if(binding.ZSystemCondition){
+    if(binding.ZSystemCondition || binding.WOHeader.ZSystemCondition){
         return binding.ZSystemCondition + " - Shutdown";
     }
 
-    return '';
+    return '-';
 }
 
