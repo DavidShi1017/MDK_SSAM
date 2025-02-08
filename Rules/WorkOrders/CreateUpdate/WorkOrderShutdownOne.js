a33a21b8dc96294a065e343f18fb81c3cfb50e53
Shi <shig@delawareconsulting.com>
2025-02-08 13:31:09 +0800

    CR 37 Shutdown function

---
+++ Rules/WorkOrders/CreateUpdate/WorkOrderShutdownOne.js
@@ -0,0 +1,13 @@
+import libCommon from '../../Common/Library/CommonLibrary';
+
+
+
+export default function WorkOrderShutdownOne(clientAPI) {
+    //Remove variable FollowUpFlagPage before create
+   
+    let binding = clientAPI.binding;
+
+    if(binding){
+
+    }
+}
