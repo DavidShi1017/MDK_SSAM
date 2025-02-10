ad4cc85d189d2c87c466c1faa99220ff407fb3ce
Shi <shig@delawareconsulting.com>
2025-02-10 13:32:17 +0800

    CR 37 Shutdown function

--- Rules/Common/DetailsPage/DetailsPageSystemCondition.js
+++ Rules/Common/DetailsPage/DetailsPageSystemCondition.js
@@ -3,14 +3,27 @@ import commonLib from '../Library/CommonLibrary';
 export default function DetailsPageSystemCondition(context) {
     let statusObject;
     let binding = context.binding;
-    if(binding.ZSystemCondition && binding.ZSystemCondition === 'X'){
-        return binding.ZSystemCondition + " - Shutdown";
+    var id ;
+    if(binding.OrderId){
+        id = binding.OrderId
     }else{
-        if(binding.WOHeader && binding.WOHeader.ZSystemCondition && binding.WOHeader.ZSystemCondition === 'X'){
-            return binding.WOHeader.ZSystemCondition + " - Shutdown";
-        }
+        id = binding.WOHeader.OrderId;
     }
+    let filterQuery = `$filter='OrderId' eq '${id}'`;
+    return context.read('/SAPAssetManager/Services/AssetManager.service', 'MyWorkOrderHeaders', [], filterQuery).then(function(result) {
+        if (result && result.length > 0) {
+           
+        }
+        return '-';
+    });
+    //if(binding.ZSystemCondition && binding.ZSystemCondition === 'X'){
+    //    return binding.ZSystemCondition + " - Shutdown";
+    //}else{
+    //    if(binding.WOHeader && binding.WOHeader.ZSystemCondition && binding.WOHeader.ZSystemCondition === 'X'){
+            return binding.WOHeader.ZSystemCondition + " - Shutdown";
+    //    }
+    //}
 
-    return '-';
+    //return '-';
 }
 
