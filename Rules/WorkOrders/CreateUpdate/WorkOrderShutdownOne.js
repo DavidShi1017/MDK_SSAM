b892d65ed48c1cea6e9532cdd69cf229d40813e8
Shi <shig@delawareconsulting.com>
2025-02-18 13:43:42 +0800

    尝试保存文件

--- Rules/WorkOrders/CreateUpdate/WorkOrderShutdownOne.js
+++ Rules/WorkOrders/CreateUpdate/WorkOrderShutdownOne.js
@@ -2,29 +2,38 @@ import libCommon from '../../Common/Library/CommonLibrary';
 
 
 import ExecuteActionWithAutoSync from '../../ApplicationEvents/AutoSync/ExecuteActionWithAutoSync';
-
+import NativeScriptObject from '../../Common/Library/NativeScriptObject';
 export default function WorkOrderShutdownOne(clientAPI) {
     //Remove variable FollowUpFlagPage before create
    
     let binding = clientAPI.binding;
 
     if(binding){
-        binding.OrderHeaderReadLink = "MyWorkOrderHeaders('" + binding.OrderId + "')";
-        //return clientAPI.executeAction('/SAPAssetManager/Actions/WorkOrders/CreateUpdate/WorkOrderShutdown.action');
-        return clientAPI.executeAction({'Name': '/SAPAssetManager/Actions/WorkOrders/CreateUpdate/WorkOrderShutdown.action', 'Properties': {
-            'Target': {
-                'EntitySet': 'MyWorkOrderHeaders',
-                'Service': '/SAPAssetManager/Services/AssetManager.service',
-                'ReadLink': binding.OrderHeaderReadLink,
-            },
-            'Properties': {
-                'ZSystemCondition': 'X'
-            },
+        // binding.OrderHeaderReadLink = "MyWorkOrderHeaders('" + binding.OrderId + "')";
+        // //return clientAPI.executeAction('/SAPAssetManager/Actions/WorkOrders/CreateUpdate/WorkOrderShutdown.action');
+        // return clientAPI.executeAction({'Name': '/SAPAssetManager/Actions/WorkOrders/CreateUpdate/WorkOrderShutdown.action', 'Properties': {
+        //     'Target': {
+        //         'EntitySet': 'MyWorkOrderHeaders',
+        //         'Service': '/SAPAssetManager/Services/AssetManager.service',
+        //         'ReadLink': binding.OrderHeaderReadLink,
+        //     },
+        //     'Properties': {
+        //         'ZSystemCondition': 'X'
+        //     },
         
-        }}).then( result => {
-            if(result){
-                return ExecuteActionWithAutoSync(clientAPI, '/SAPAssetManager/Actions/CreateUpdateDelete/UpdateEntitySuccessMessage.action');
-            }
+        // }}).then( result => {
+        //     if(result){
+        //         return ExecuteActionWithAutoSync(clientAPI, '/SAPAssetManager/Actions/CreateUpdateDelete/UpdateEntitySuccessMessage.action');
+        //     }
+        // });
+
+        let documentPath = '/storage/emulated/0/MDKApp/data.json'; // 共享目录
+        let jsonData = { "name": "David", "age": 30, "city": "Shanghai" };
+        
+        let content = JSON.stringify(jsonData, null, 2);
+        var documentFileObject = NativeScriptObject.getNativeScriptObject(clientAPI).fileSystemModule.File.fromPath(documentPath);
+        documentFileObject.writeSync(content, () => {
+            return clientAPI.executeAction('/SAPAssetManager/Actions/Documents/DownloadMediaFailure.action');
         });
     }
 }
