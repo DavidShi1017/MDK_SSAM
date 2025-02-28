61066e677e1e854a185319fc81acdd490605d201
shig <shig@delawareconsulting.com>
2025-02-28 13:19:45 +0800

    尝试修复NotRunning点击后其他项目无法修改问题

--- Rules/InspectionCharacteristics/InspectionCharacteristicsNotRunningAll.js
+++ Rules/InspectionCharacteristics/InspectionCharacteristicsNotRunningAll.js
@@ -140,7 +140,5 @@ export default async function InspectionCharacteristicsNotRunningAll(context) {
     
     //context.getPageProxy().redraw();
     
-    // return new Promise(resolve => {
-    //     resolve({ "Success": true });
-    // });
+    context.executeAction('/SAPAssetManager/Actions/Common/AppUpdateSuccess.action');
 }
