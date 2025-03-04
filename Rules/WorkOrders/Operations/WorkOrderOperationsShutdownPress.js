f0c63813770011468534100f5274da02b46bb960
shig <shig@delawareconsulting.com>
2025-03-04 16:10:56 +0800

    解决从首页open进入list页面，shutdown无效问题

--- Rules/WorkOrders/Operations/WorkOrderOperationsShutdownPress.js
+++ Rules/WorkOrders/Operations/WorkOrderOperationsShutdownPress.js
@@ -10,11 +10,15 @@ export default function WorkOrderOperationsShutdownPress(context) {
     
 
     const selectedOperations = libCommon.getStateVariable(context, 'selectedOperations');
-    
+           
     if (selectedOperations.length === 0) {
         //context.getPageProxy().dismissActivityIndicator();
         return context.executeAction('/SAPAssetManager/Actions/WorkOrders/Operations/WorkOrderOperationsNoSelectedMessage.action');
     }else{
+        let actionBinding = {
+            selectedOperations,
+        };
+        context.getPageProxy().setActionBinding(actionBinding);
         return context.executeAction(
             {
                 'Name': '/SAPAssetManager/Actions/Common/GenericWarningDialog.action',
