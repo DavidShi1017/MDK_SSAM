cb39e661ad9340d3badf53b9cf9628b2505b58fc
shig <shig@delawareconsulting.com>
2025-03-05 10:14:41 +0800

    解决从首页open进入list页面，shutdown无效问题

--- Rules/WorkOrders/Operations/WorkOrderOperationListViewOnLoad.js
+++ Rules/WorkOrders/Operations/WorkOrderOperationListViewOnLoad.js
@@ -5,8 +5,8 @@ import { ResetBulkConfirmationQueue } from './BulkConfirmationQueue';
 
 export default function WorkOrderOperationListViewOnLoad(clientAPI) {
     ResetBulkConfirmationQueue(clientAPI);
-    if(!context.binding){
-        context.getPageProxy().setBinding({'key': 123});
+    if(!clientAPI.binding){
+        clientAPI.getPageProxy().setBinding({'key': 123});
     }
     setCaption(clientAPI).then(() => {
         let myOperationListView = libCommon.getStateVariable(clientAPI, 'MyOperationListView');
